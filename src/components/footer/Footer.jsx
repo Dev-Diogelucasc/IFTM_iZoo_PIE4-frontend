1a5619ed5af0691de2dbe459582a79be9f31d70c
Diogenes Cassimiro <dev.diogelucas@gmail.com>
2025-10-02 22:04:54 -0300

    feat:update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-stone-300 border-t border-b shadow-2xl mb-3 bg-[#F8F8F8] py-4 mt-8">
+    <footer className="w-full border-stone-300 border-t border-b shadow-2xl mb-1 bg-[#F8F8F8] py-4 mt-8">
       <div className="max-w-6xl mx-auto px-4 text-center text-gray-600 text-sm">
         © {new Date().getFullYear()}  Izoo. Todos os direitos reservados.
       </div>
